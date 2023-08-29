82f5d5c9c1e3724e50932f7ab1ad8afbc12a910d
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-08-29 11:58:19 +0300

    added root route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const uploadRoutes = require('./routes/uploadRoutes');
+const rootRoutes = require('./routes/rootRoutes');
 
 app.use(express.json());
 app.use(uploadRoutes);
+app.use(rootRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -5,4 +5,5 @@ const router = express.Router();
 
 router.post('/upload', uploadController.uploadImage);
 
+
 module.exports = router;
