ef66f1c15f5fe3c80833659efc62794a5e2ddba6
Aleksandra Davydova <cyberberry.w@gmail.com>
2023-09-01 00:49:22 +0300

    removed acl

--- src/controllers/getImageURL.js
+++ src/controllers/getImageURL.js
@@ -13,8 +13,6 @@ exports.getSignedUrl = async (req, res) => {
             Bucket: process.env.BUCKET_NAME || 'bucketdockerkryvoboktest',
             Key: `cards/test/${cardID}/${fileName}`,
             Expires: 86400, // на сутки
-            ACL: 'public-read',
-
         };
 
         const signedUrl = await s3.getSignedUrl('getObject', params);
